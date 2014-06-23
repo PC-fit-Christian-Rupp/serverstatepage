e3d2b5590a735f5d73a191bcc00dcce23c93d8d3
Christian Rupp <privat@christian-rupp.de>
2014-06-23 13:04:12 +0200

    new functions in service and HTTP

---
+++ code/service.py
@@ -0,0 +1,14 @@
+
+
+class service:
+
+	def __init__(self, port = None, name = None):
+		if port && port > 0:
+			self.__port = port
+		else:
+			#TODO raise exception
+			return None
+		if name:
+			self.__name = name
+		else:
+			self.__name = 'Service Name missing'
