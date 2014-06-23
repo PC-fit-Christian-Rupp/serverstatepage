016b750d1476f7c58ad174d5f0a12fc7ef4c04ef
Christian Rupp <privat@christian-rupp.de>
2014-06-23 11:56:03 +0200

    Functions added to server.py

--- code/server.py
+++ code/server.py
@@ -10,3 +10,19 @@ class server:
 			self.__name = name
 		else:
 			self.__name = 'Missing Server Name'
+		self.__color = '#FFFF00'
+
+	def getName(self):
+		return self.__name
+
+	def scan(self):
+		response = os.system("ping -c 1 " + str(self.__ip))
+		if response == 0:
+			self.__color = '#00FF00'
+		else:
+			self.__color = '#FF0000' 
+		return response
+
+	def getColot(self):
+		return self.__color
+
