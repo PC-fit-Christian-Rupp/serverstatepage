7354e97aad7b4445d7aa2cb9d8e5d0d1e8c9c6ff
Christian Rupp <privat@christian-rupp.de>
2014-06-23 18:11:11 +0200

    Bugfixes

--- code/loadConfig.py
+++ code/loadConfig.py
@@ -20,11 +20,11 @@ class loadConfig:
 		return self.__cfg[self.__default]['CountServer']
 
 	def __readData(self):
-		for i in self.__countServer:
+		for i in int(self.__countServer)-1:
 			self.__loadServer(i)
 
 	def __loadServer(self, numb):
-		tmp = server.server(self.__cfg[self.__default][str(num)+'.IP'],self.__cfg[self.__default][str(numb)+'.Name'])
+		tmp = server.server(self.__cfg[self.__default][str(numb)+'.IP'],self.__cfg[self.__default][str(numb)+'.Name'])
 		for i in self.__cfg[self.__default][str(numb)+'.CountService']:
 			self.__loadService(i, tmp)
 		self.__lst.add(tmp)
