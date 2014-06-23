3ce18aafe7141e761af746806022edd93c768475
Christian Rupp <privat@christian-rupp.de>
2014-06-23 11:10:49 +0200

    New class files, currently empty

--- code/loadConfig.py
+++ code/loadConfig.py
@@ -3,5 +3,13 @@ import configparser
 class loadConfig:
 
 	def __init__(self):
+		self.__default = 'DEFAULT'
+		self.__loadConfig()
+		self.__countServer = self.__loadCountServer()
+
+	def __loadConfig(self):
 		self.__cfg = configparser.ConfigParser()
 		self.__cfg.read('config.ini')
+
+	def __loadCountServer(self):
+		return self.__cfg[self.__default]['CountServer']
