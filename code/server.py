c3859631090db7839dbada471f3a57b3f3807828
Christian Rupp <privat@christian-rupp.de>
2014-06-23 12:47:32 +0200

    state added to server

--- code/server.py
+++ code/server.py
@@ -11,6 +11,7 @@ class server:
 		else:
 			self.__name = 'Missing Server Name'
 		self.__color = '#FFFF00'
+		self.__state = 1
 
 	def getName(self):
 		return self.__name
@@ -18,9 +19,11 @@ class server:
 	def scan(self):
 		response = os.system("ping -c 1 " + str(self.__ip))
 		if response == 0:
+			self.__state = response
 			self.__color = '#00FF00'
 		else:
-			self.__color = '#FF0000' 
+			self.__color = '#FF0000'
+			self.__state = 2
 		return response
 
 	def getColot(self):
