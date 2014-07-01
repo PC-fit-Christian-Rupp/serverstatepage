00d6750030d9876ba5e1338365a8b404a2172b8d
Christian Rupp <privat@christian-rupp.de>
2014-07-01 18:25:37 +0200

    state added to service

--- code/service.py
+++ code/service.py
@@ -6,6 +6,10 @@ class service:
 		self.__port = cfg[pfx+'SERVICE'][i+'Port']
 		self.__name = cfg[pfx+'SERVICE'][i+'Name']
 		self.__countObjects = cfg[pfx+'SERVICE'][i+'CountObjects']
+		self.__state = 1
 		if self.__countObjects > 0:
 			self.__readData(cfg)
+
+	def getState(self):
+		return self.__state
 
