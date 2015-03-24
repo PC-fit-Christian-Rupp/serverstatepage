dba28946f3a4a20fe0b51ff7aa3db225e8e9364e
Christian Rupp <privat@christian-rupp.de>
2015-03-24 19:00:34 +0100

    Refactoring Main

--- code/testroutine/main.py
+++ code/testroutine/main.py
@@ -5,12 +5,19 @@ class main:
 	
 	def __init__(self, path = None):
 		self.__cfg = configparser.ConfigParser()
-		if path:
-			self.__cfg.read(path)
-		else:
-			self.__cfg.read("conf.ini")
+		self.__path = "conf.ini"
+
+	def parsearg(self, argv):
+		pass
+
+	def readConfig(self):
+		self.__cfg.read(self.__path)
 
 if __name__ == "__main__":
+	Main = main()
+	Main.parsearg(sys.argv)
+	Main.readConfig()
+	Main.loadDefault()
 	if len(sys.argv)==2:
 		path = sys.argv[1]
 		if (os.path.exists(path))
