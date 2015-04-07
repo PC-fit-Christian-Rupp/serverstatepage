9de440aa09ad836fbaf88dc51951331b765cd78f
Christian Rupp <privat@christian-rupp.de>
2015-04-07 17:23:08 +0200

    All Server datamodel stubs added.

---
+++ code/testroutine/datamodel/server.py
@@ -0,0 +1,44 @@
+class server:
+
+	def __init__ (self, Name, Ip):
+		self.__Name = Name
+		self.__Ip = Ip
+
+	def addService (self, Service):
+		pass
+
+	def check (self):
+		pass
+
+	def __test(self):
+		pass
+
+	def __checkOs(self):
+		pass
+
+	def __checkVersionMac(self):
+		pass
+
+	def __checkVersionLinux(self):
+		pass
+
+	def __checkVersionWin(self):
+		pass
+
+	def getOs(self):
+		pass
+
+	def getVersion(self):
+		pass
+
+	def getList(self):
+		pass
+
+	def getState(self):
+		pass
+
+	def getName(self):
+		pass
+
+	def getIp(self):
+		return self.__Ip
