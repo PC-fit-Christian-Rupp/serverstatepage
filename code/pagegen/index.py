5fd9de7e2b7b57640c2f2bc5d09bb6da857bce67
Christian Rupp <privat@christian-rupp.de>
2015-05-10 12:10:16 +0200

    argv function stub added!

--- code/pagegen/index.py
+++ code/pagegen/index.py
@@ -1,8 +1,16 @@
 import cgi
+import ccookie
+from string import Template
+import sys
 
 class index:
 
 	def __init__(self):
 		pass
 
+	def parseArg(self, argv):
+		pass
+
+if __name__ == "__main__":
+	a = index.index()
 
