5ec7a0ad0d8ce2e1e21da157a4bb6b3cb98cfdb3
Christian Rupp <privat@christian-rupp.de>
2015-05-12 12:05:02 +0200

    New Functions implemented at index.py

--- code/pagegen/index.py
+++ code/pagegen/index.py
@@ -2,17 +2,51 @@ import cgi
 import ccookie
 from string import Template
 import sys
+import configparser
+import os
 
 class index:
 
 	def __init__(self):
+		self.__cfg = configparser.ConfigParser()
+		self.__loadConfigFile()
+		self.__cookieExist()
+		self.__cookie = ccookie.ccookie()
+
+	def loadConfig(self):
+		self.__loadConfigFile()
+		self.__loadDefaultConf()
+		self.__loadIndexConf()
+
+	def __cookieExist(self):
+		if 'HTPP_COOKIE' in os.environ:
+			self.__cookieExist = 1
+		else:
+			self.__cookieExist = 0
+
+	def useCookie(self):
 		pass
 
-	def __loadDefaultConf(self):
+	def __loadConfigFile(self):
+		self.__cfg.read('conf.ini')
+
+	def __loadDefaultConf(self, section):
 		pass
 
 	def __loadIndexConf(self):
 		pass
+
+	def loadElements(self):
+		pass
+
+	def render(self):
+		pass
+
+	def printPage(self):
+		pass
+
+	def getPage(self):
+		return self.__page
 
 	def parseArg(self, argv):
 		if len(argv)>1:
@@ -27,4 +61,5 @@ class index:
 
 if __name__ == "__main__":
 	a = index.index()
-
+	a.parseArg(sys.argv)
+	
