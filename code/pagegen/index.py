dff307bbef751b811540dd536c89e137975ce8e2
Christian Rupp <privat@christian-rupp.de>
2015-05-18 12:48:06 +0200

    Fix for run!

--- code/pagegen/index.py
+++ code/pagegen/index.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import cgi
 import ccookie
 from string import Template
@@ -27,17 +28,29 @@ class index:
 			self.__cookieExist = 0
 
 	def useCookie(self):
-		pass
+		if self.__useCookie:
+			pass
 
 	def __loadConfigFile(self):
 		self.__cfg.read('conf.ini')
 
 	def __loadDefaultConf(self, section):
 		for key in self.__cfg[section]:
-			pass
+			if key == 'pfx':
+				self.__pfx = self.__getConfItem(section, key)
+			elif key == 'trp':
+				self.__trp = self.__getConfItem(section, key)
+			elif key == 'cookie':
+				self.__useCookie = int(self.__getConfItem(section, key))
+			elif key == 'mysql':
+				self.__mysql = int(self.__getConfItem(section, key))
+
+	def __getConfItem(self, section, key):
+		return self.__cfg[section][key]
 
 	def __loadIndexConf(self):
-		pass
+		section = 'INDEX'
+		self.__title = self.__getConfItem(section, 'title')
 
 	def loadElements(self):
 		pass
