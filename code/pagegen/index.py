31a0ba6b47975cad9c430bab8b53d9a2c56ec737
Christian Rupp <privat@christian-rupp.de>
2015-06-17 15:54:46 +0200

    Headertitle added to render Function! Configuration refactored to an new class!

--- code/pagegen/index.py
+++ code/pagegen/index.py
@@ -3,8 +3,8 @@ import cgi
 import ccookie
 from string import Template
 import sys
-import configparser
 import os
+import conf
 
 import cgitb
 cgitb.enable()
@@ -12,55 +12,16 @@ cgitb.enable()
 class index:
 
 	def __init__(self):
-		self.__cfg = configparser.ConfigParser()
-		self.__cookieExist()
-		self.__pfx = None
-		self.__useCookie = 0
-
-	def loadConfig(self):
-		self.__loadConfigFile()
-		self.__loadDefaultConf('DEFAULT')
-		self.__loadIndexConf()
-
-	def __cookieExist(self):
-		if 'HTPP_COOKIE' in os.environ:
-			self.__cookieExist = 1
-		else:
-			self.__cookieExist = 0
-
-	def useCookie(self):
-		if self.__useCookie:
-			pass
-
-	def __loadConfigFile(self):
-		self.__cfg.read('conf.ini')
-
-	def __loadDefaultConf(self, section):
-		for key in self.__cfg[section]:
-			if key == 'pfx':
-				self.__pfx = self.__getConfItem(section, key)
-			elif key == 'trp':
-				self.__trp = self.__getConfItem(section, key)
-			elif key == 'cookie':
-				self.__useCookie = int(self.__getConfItem(section, key))
-			elif key == 'mysql':
-				self.__mysql = int(self.__getConfItem(section, key))
-
-	def __getConfItem(self, section, key):
-		return self.__cfg[section][key]
-
-	def __loadIndexConf(self):
-		section = 'INDEX'
-		for key in self.__cfg[section]:
-			if key == 'title':
-				self.__title = self.__getConfItem(section, key)
+		self.__cfg = conf.conf()
+		self.__pageValues = self.__cfg.getIndex()
 
 	def loadElements(self):
 		pass
 
 	def render(self):
+		self.__header = self.__header.substitute(headertitle=self.__pageValues[1])
 		self.__body = self.__body.substitute(header=self.__header, wrapper=self.__contentwrapper, footer=self.__footer)
-		self.__page = self.__page.substitute(body=self.__body, title = self.__title)
+		self.__page = self.__page.substitute(body=self.__body, title = self.__pageValues[0])
 
 	def printPage(self):
 		self.__page.replace('\n',' ')
@@ -82,8 +43,6 @@ class index:
 
 if __name__ == "__main__":
 	a = index()
-	a.loadConfig()
-	a.useCookie()
 	a.connectData()
 	a.loadTemplates()
 	a.loadElements()
